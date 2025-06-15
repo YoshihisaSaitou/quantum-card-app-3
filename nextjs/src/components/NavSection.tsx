92a4ff3b7c81b07975247c60d91c47d605c9d255
Yoshihisa Saito <anti_viral_drug@yahoo.co.jp>
2025-06-15 16:43:27 +0900

    Created github lint config.

--- nextjs/src/components/NavSection.tsx
+++ nextjs/src/components/NavSection.tsx
@@ -8,7 +8,7 @@ export default function NavSection({ onShuffle }: NavSectionProps) {
     return (
         <div className="absolute top-0 right-0">
             <nav>
-                <ul className="flex gap-4 p-4">
+                <ul className="flex flex-col gap-4 p-4">
                     <li
                         className="cursor-pointer hover:text-blue-500 transition-colors"
                         onClick={onShuffle}
