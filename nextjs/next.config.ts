09a35f665659fa3eb4b51b43f8bafe111655049a
Yoshihisa Saito <anti_viral_drug@yahoo.co.jp>
2025-07-13 10:03:34 +0900

    The build method has been modified so that it can be viewed on GitHub Pages.

--- nextjs/next.config.ts
+++ nextjs/next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
 
+const repo = 'quantum-card-app-3';
+
 const nextConfig: NextConfig = {
     output: "export", // 静的エクスポートを有効化
     trailingSlash: true, // 必要に応じてURLの末尾にスラッシュを追加
+    basePath: `/${repo}`, // ベースパスを設定
+    assetPrefix: `/${repo}/`, // 静的ファイルのパスを設定
     images: {
         unoptimized: true, // next/imageの最適化を無効化
     },
