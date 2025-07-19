cde3223e9df44f484f9f3ab5f971c5779602ccde
Yoshihisa Saito <anti_viral_drug@yahoo.co.jp>
2025-07-19 09:49:45 +0900

    The build method has been modified.

--- nextjs/eslint.config.mjs
+++ nextjs/eslint.config.mjs
@@ -10,6 +10,18 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+    // 除外
+    {
+        ignores: [
+            ".next/**",   // Next.js ビルド成果物
+            "dist/**",    // CommonJS／Rollup 出力
+            "out/**",     // 静的書き出し
+            "docs/**",     // GitHub Pages用の静的書き出し
+            "build/**"    // その他ビルド成果物
+        ],
+    },
+
+    // 既存の Next.js + TypeScript 推奨設定
     ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
